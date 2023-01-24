27fd7206258ceb6f044cee230ca73acb5ae5efc9
karoll1naa <karollinnajan@gmail.com>
2023-01-24 03:16:04 +0200

    Correction of names

--- exams/sum.cpp
+++ exams/sum.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main() {
     int i;
     float Sum = 0, A[i];
-    cout<<"Enter size Array = "<<endl;
+    cout<<"Enter the number of array elements = "<<endl;
     cin>>i;
-    cout<<"Array: "<<endl;
+    cout<<"Fill the array with numbers: "<<endl;
     for (int j = 0; j < i; j++){
         cin>>A[i];
         Sum+= A[i];
     }
     cout<<"Sum: "<<endl;
-    cout << Sum <<endl;
+    cout<<Sum<<endl;
 
 }
